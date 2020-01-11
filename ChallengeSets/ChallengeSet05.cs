bc4019b946a7d590145240cc550acc5ee566fb81
Jordan <jjwilli13@gmail.com>
2020-01-10 18:15:06 -0600

    First commit in ChallengeSet05

--- ChallengeSets/ChallengeSet05.cs
+++ ChallengeSets/ChallengeSet05.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace ChallengeSets
@@ -7,17 +8,40 @@ namespace ChallengeSets
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+
+            return ((startNumber / n) + 1) *n;
+
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
-        }
+            foreach (var business in businesses)
+            {
+                if (business.TotalRevenue <= 0)
+                {
+                    business.Name = "CLOSED";
+                }
+            }
 
+        }
+ 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] > numbers[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
@@ -27,7 +51,27 @@ namespace ChallengeSets
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+            string sentence = "";
+
+            foreach (var word in words)
+            {
+                if (word.Trim().Length > 0)
+                {
+                    sentence += word.Trim() + " ";
+                }
+            }
+            if (sentence.Length == 0)
+            {
+                return "";
+            }
+            sentence = sentence.Substring(0, sentence.Length - 1);
+            sentence += ".";
+            return sentence;
+
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
@@ -37,7 +81,9 @@ namespace ChallengeSets
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
+
             throw new NotImplementedException();
+
         }
     }
 }
